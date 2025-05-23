3529c3de3254d32d52ecfaa633e3d5375dc11405
eorozcoo <eorozco@uoregon.edu>
2025-05-23 00:10:50 -0700

    Publish Event Buttton and Other

--- frontend/static/event_editor.js
+++ frontend/static/event_editor.js
@@ -3,12 +3,24 @@ const task = document.getElementById("event_editor_container");
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector('.dropdown-toggle');
     const detailsContent = document.querySelector('.details-content');
-
+    const publishCheckbox = document.getElementById('publishCheckbox');
+    const submitButton = document.getElementById('submitButton');
+  
     if (toggleButton && detailsContent) {
-        toggleButton.addEventListener('click', () => {
-            detailsContent.classList.toggle('hidden');
-        });
-    } else {
-        console.error('Required elements are missing in the DOM.');
+      toggleButton.addEventListener('click', () => {
+        detailsContent.classList.toggle('hidden');
+      });
     }
-});
+  
+  // Update button label based on checkbox
+  if (publishCheckbox && submitButton) {
+    publishCheckbox.addEventListener('change', () => {
+      if (publishCheckbox.checked) {
+        submitButton.textContent = "Save and Publish";
+      } else {
+        submitButton.textContent = "Save Event";
+      }
+    });
+  }
+  });
+  
